497388b0113fb2307328999b8b8ad840121e1237
Aayush Goel <aayushg2017@email.iimcal.ac.in>
2016-07-17 15:04:56 +0000

    Added parameter based question fetching

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,16 +6,16 @@ class WelcomeController < ApplicationController
   def practice
     @question = Question.find(1)
     @numquestions = @question.questiontext.split(";").size
-    @initquestion = @question.questiontext.split(";")[0]
+    @initquestion = @question.questiontext.split(";")[params[:question].to_f]
   end
 
   def check
     @question = Question.find(params[:answerform][:questionnum])
     @keyword = @question.keywordtext.split(";")[0]
     if @keyword == params[:answerform][:answer]
-      render 'index'
+      redirect_to practice_path(:question => 1)
     else
-      redirect_to practice_path
+      redirect_to practice_path(:question => 0)
     end
 
   end
